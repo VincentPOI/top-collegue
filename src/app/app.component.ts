9117013547871950c53112bab84666594e6f88a3
vincent <vincentpoisson08gmail.com>
2018-02-14 17:44:23 +0100

    tp 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Collegue } from './shared/domain/collegue'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  collegues:Collegue[]
+  cachee:boolean
+  ngOnInit() {
+    this.cachee = true
+    this.collegues= new Array()
+    this.collegues.push(new Collegue("jambon","https://images-na.ssl-images-amazon.com/images/I/411RzrsBmBL._SY300_.jpg",20))
+    this.collegues.push(new Collegue("Ait-Ahmed","https://images-na.ssl-images-amazon.com/images/I/411RzrsBmBL._SY300_.jpg",19))
+    this.collegues.push(new Collegue("poisson","https://images-na.ssl-images-amazon.com/images/I/411RzrsBmBL._SY300_.jpg",18))
+    this.collegues.push(new Collegue("timon","https://images-na.ssl-images-amazon.com/images/I/411RzrsBmBL._SY300_.jpg",17))
+    this.collegues.push(new Collegue("pumba","https://images-na.ssl-images-amazon.com/images/I/411RzrsBmBL._SY300_.jpg",16))
+  }
+
+  add(pseudo:HTMLInputElement, imageUrl: HTMLInputElement,alert: HTMLDivElement) {
+    this.collegues.push(new Collegue(pseudo.value,imageUrl.value,0))
+    pseudo.value =""
+    imageUrl.value=""
+    this.cachee = false
+    return false
+  }
 }
---
+++ src/app/un-collegue/un-collegue.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { Collegue } from '../shared/domain/collegue';
+
+@Component({
+  selector: 'app-un-collegue',
+  templateUrl: './un-collegue.component.html',
+  styleUrls: ['./un-collegue.component.css']
+})
+export class UnCollegueComponent implements OnInit {
+
+  @Input() collegue:Collegue;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  jaime() {
+    this.collegue.score += 10
+    return false
+  }
+  jedeteste() {
+    this.collegue.score -= 5
+    return false
+  }
+
+}
